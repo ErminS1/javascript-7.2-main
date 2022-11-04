2ac98992eec30a182544cb327f68aab33f8b4f49
ErminS1 <erminklein@gmail.com>
2022-11-04 11:23:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function opdracht72(){
+    const budget = 100;
+    var product = prompt ("voer uw product in","");
+
+    if(budget>product){
+        alert("u heeft genoeg geld!")
+        console.log('U heeft genoeg geld!')
+    }
+    
+    else{
+        console.log("u heeft niet genoeg geld")
+        alert("u heeft niet genoeg geld")
+    }
+    }
